c1865073a971f7294b262349400465f5131cdc06
EvaAdriana <evaarredondo19@hotmail.com>
2014-06-27 16:17:59 -0500

    primer commit

---
+++ Practica1/guardar.php
@@ -0,0 +1,23 @@
+<?php
+	echo "Captura de Resultados";
+	echo "<br />";
+	echo "<br />";
+	echo "Goles Equipo Local:   " . $_POST['equipo1_local'];
+	echo "<br />";
+	echo "Goles Equipo Visitante:   " . $_POST['equipo2_visitante'];
+	echo "<br />";
+	echo "<br />";
+	echo "Goles Equipo Local:   " . $_POST['equipo3_local'];
+	echo "<br />";
+	echo "Goles Equipo Visitante:   " . $_POST['equipo4_visitante'];
+	echo "<br />";
+	echo "<br />";
+	echo "Equipo Local:   " . $_POST['equipo_local'];
+	echo "<br />";
+	echo "Goles Equipo Local:   " . $_POST['equipo5_local'];
+	echo "<br />";
+	echo "Goles Equipo Visitante:   " . $_POST['equipo6_visitante'];
+	echo "<br />";
+	echo "Equipo Visitante:   " . $_POST['equipo_visitante'];
+	
+?>
